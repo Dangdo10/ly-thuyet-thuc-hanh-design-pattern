24ad210f77182174f82ecf90d01aba70d1f1b0f7
Dang Van Do <dotdtasia102022@gmail.com>
2024-12-19 13:40:58 +0700

    update observer

--- src/main/java/org/example/behavioral/observer/Mailer.java
+++ src/main/java/org/example/behavioral/observer/Mailer.java
@@ -4,6 +4,11 @@ public class Mailer implements Observer {
 
     @Override
     public void update(User user) {
+
+        if (user.getStatus() == LoginStatus.SUCCESS) {
+            System.out.println("Mailer: User " + user.getEmail() + " is login success. An email was sent!");
+        }
+
         if (user.getStatus() == LoginStatus.EXPIRED) {
             System.out.println("Mailer: User " + user.getEmail() + " is expired. An email was sent!");
         }
