e247e881df5823443b806b5e185dc2df57a128aa
Dang Van Do <dotdtasia102022@gmail.com>
2024-12-20 17:20:35 +0700

    ly thuyet va vi du prototype pattern

---
+++ src/main/java/org/example/creational/prototype/vidu1/ITApp.java
@@ -0,0 +1,14 @@
+package org.example.creational.prototype.vidu1;
+
+public class ITApp {
+
+    public static void main(String[] args) {
+        Computer computer1 = new Computer(
+                "Window10", "Word", "Bkav",
+                "Chrome", "Skype");
+        Computer computer2 = computer1.clone();
+        computer2.setOthers("Eclipse, Facebook");
+        System.out.println(computer1);
+        System.out.println(computer2);
+    }
+}
