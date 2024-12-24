4b07115da07d20ca6122722b4fabf5155b7ec045
Dang Van Do <dotdtasia102022@gmail.com>
2024-12-24 14:45:27 +0700

    lý thuyết và ví dụ proxy pattern

---
+++ src/main/java/org/example/structural/proxy/protection_proxy/UserService.java
@@ -0,0 +1,8 @@
+package org.example.structural.proxy.protection_proxy;
+
+public interface UserService {
+
+    void load();
+
+    void insert();
+}
---
+++ src/main/java/org/example/structural/proxy/virtual_proxy/Image.java
@@ -0,0 +1,5 @@
+package org.example.structural.proxy.virtual_proxy;
+
+public interface Image {
+    void showImage();
+}
